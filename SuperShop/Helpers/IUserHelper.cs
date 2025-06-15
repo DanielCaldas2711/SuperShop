5f6d2be50c50e63a14017ba262b37fff2801e3d8
DanielCaldas2711 <caldasdaniel12@gmail.com>
2025-06-15 18:27:22 +0100

    Change user profile and password

--- SuperShop/Helpers/IUserHelper.cs
+++ SuperShop/Helpers/IUserHelper.cs
@@ -15,5 +15,9 @@ namespace SuperShop.Helpers
 
         Task LogOutAsync();
 
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
     }
 }
